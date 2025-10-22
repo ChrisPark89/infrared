1c24aaf0aaa02f2c9098cede8402dff1405488a4
Jeonghyuk Park <jeonghyuk.park@ibm.com>
2025-10-22 10:46:52 +0100

    change start.py/add test server

--- start.py
+++ start.py
@@ -1,66 +1,41 @@
 import RPi.GPIO as GPIO
-import sys
-import getopt
-import curses
 import time
-import os
-import termios
-from multiprocessing import Process, Queue, Lock
-
 import signal
+import requests
+import keyboard  # Import keyboard module to capture key presses
 
 def Exit_gracefully(signal, frame):
-    return 0
+    GPIO.cleanup()
+    print("Exiting gracefully...")
+    exit(0)
 
 signal.signal(signal.SIGINT, Exit_gracefully)
 signal.signal(signal.SIGTSTP, Exit_gracefully)
 
-globals()['user']=""
-
-def user_input(stdin):
-    while 1:
-        response = ""
-        while (response != "hursley"):
-            print("Password:", end='')
-            sys.stdout.flush()
-            termios.tcflush(stdin, termios.TCIOFLUSH)
-            response = stdin.readline()
-            response = response.replace("\n","")
-            print (response)
-            if (response != "hursley"):
-                print("Incorrect password, try again......")
-
-        print ("Welcome admin. Your code is 341")
-
-def startp():
-    newstdin = os.fdopen(os.dup(sys.stdin.fileno()))    
-    q = Queue()
-    globals()['user'] = Process(target=user_input, args=(newstdin,))
-    globals()['user'].daemon = True
-    globals()['user'].start()
+def send_http_request():
+    try:
+        server_url = '127.0.0.1' # Change this address to the scoreboard server address
+        response = requests.post(server_url, json={"sensor_triggered": True}) # Change the json format to the server's spec
+        
+        if response.status_code == 200:
+            print("HTTP request sent successfully.")
+        else:
+            print(f"Failed to send HTTP request. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error occurred while sending HTTP request: {e}")
 
 def gpio_callback(channel):
-    if (GPIO.input(channel) == 1):
-        globals()['user'].terminate()
-        
-        for i in range(0,100):
-            print ("")
+    if (GPIO.input(channel) == 1): # sensor gets triggered
+        print("Sensor triggered! Sending HTTP request...")
+        send_http_request()
 
-        for i in range(50,0,-1):
-            print ("--------------- ALARM: Terminal approached. Waiting (" + str(i) + ").... -------------")
-            time.sleep(1)
-        
-        startp()
+ch = 5
+debounce = 200
 
-startp()
-
-ch=5
-debounce=200
-
-# setup GPIO call back
+# setup GPIO call back for sensor
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(ch,GPIO.IN)
-GPIO.add_event_detect(ch,GPIO.BOTH,callback=gpio_callback, bouncetime=debounce)
+GPIO.add_event_detect(ch, GPIO.BOTH, callback=gpio_callback, bouncetime=debounce)
     
 while 1:
-    x=1
+    time.sleep(0.5)
